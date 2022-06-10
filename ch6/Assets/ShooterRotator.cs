ee398d991adc55cc20799789efa6fcec80a2f506
nmsohn <odna425@gmail.com>
2022-06-10 23:01:09 +1200

    Add ch6

---
+++ ch6/Assets/ShooterRotator.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class ShooterRotator : MonoBehaviour
+{
+    private enum RotateState
+    {
+        Idle,
+        Vertical,
+        Horizontal,
+        Ready
+    }
+
+    private RotateState state = RotateState.Idle;
+
+    public float verticalRotateSpeed = 360f;
+    public float horizontalRotateSpeed = 360f;
+
+    void Update()
+    {
+        if (state == RotateState.Idle)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                state = RotateState.Horizontal;
+            }
+            else if (state == RotateState.Horizontal)
+            {
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Vertical;
+                }
+            }
+            else if (state == RotateState.Vertical)
+            {
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0,0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Ready;
+                }
+            }
+        }
+    }
+}
